5637e9db061ae2e2c0a7c4f075dff50181f21457
Anderson <anderson.pessoa02@outlook.com>
2017-06-23 13:49:15 -0300

    Initial Comit

---
+++ ContarOcorrencias.py
@@ -0,0 +1,37 @@
+#imports
+import urllib.request;
+import json;
+from flask import Flask, request, render_template, url_for, redirect;
+
+#Definindo o App
+app = Flask(__name__);
+
+#Rota da página inicial
+@app.route('/')
+def index():
+	#Renderiza o template index.html
+	return render_template('index.html');
+
+#Rota da API
+@app.route('/api/ocorrencias')
+def contarOcorrencias():
+	try:
+		#Obter queryParameters
+		url = request.args.get('url');
+		criterio = request.args.get('criterio');
+		#Html do site obtido através dessa url
+		htmlDoSite = urllib.request.urlopen(url).read();
+		#Converter para string
+		htmlDoSite = str(htmlDoSite);
+		#Contar o número de ocorrências
+		ocorrencias = htmlDoSite.count(criterio);
+	except Exception as e:
+		#Se ocorrer um erro, ocorrências recebe -1
+		ocorrencias = -1;
+	
+	#Retorna um json com as ocorrências da palavra
+	return json.dumps({'ocorrencias': ocorrencias});
+
+#Executar a aplicação
+if __name__ == '__main__':
+	app.run(debug = True);
